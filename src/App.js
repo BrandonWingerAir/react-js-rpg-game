586c67f818f2095c533bec225487ee976eff709d
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-08-14 14:10:41 -0400

    setup battle menu scene component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { StartMenu } from './components/StartMenu/StartMenu';
 import { useState } from 'react';
 import './App.css';
+import { StartMenu } from './components/StartMenu/StartMenu';
+import { BattleMenu } from './components/BattleMenu/BattleMenu';
 
 export const App = () => {
   const [scene, setScene] = useState('start');
@@ -8,7 +9,7 @@ export const App = () => {
   return (
     <div className="App">
       {scene === 'start' && <StartMenu onStartClick={() => setScene('battle')}/>}
-      {scene === 'battle' && <>Battle Scene</>}
+      {scene === 'battle' && <BattleMenu/>}
       {scene === 'gameOver' && <>Game Over</>}
     </div>
   );
