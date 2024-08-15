5f030df61743995b4d00e4dbc2d28934313b940e
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-08-15 17:14:44 -0400

    characters, player summary & health bar component

--- src/components/PlayerSummary/PlayerSummary.js
+++ src/components/PlayerSummary/PlayerSummary.js
@@ -1,15 +1,23 @@
 import styles from './styles.module.css';
+import { HealthBar } from '../HealthBar/HealthBar';
 
 const red = '#821200';
 const blue = '#1953cb';
 
-export const PlayerSummary = ({ main = false }) => {
+export const PlayerSummary = ({ main = false, name, level, health, maxHealth }) => {
     return (
         <div 
             style={{ backgroundColor: main ? red : blue }} 
             className={styles.main}
         >
-            Player Summary Component
+            <div className={styles.info}>
+                <div className={styles.name}>{name}</div>
+                <div className={styles.level}>Lv. {level}</div>
+            </div>
+
+            <div className={styles.health}>
+                <HealthBar/>
+            </div>
         </div>
     );
 }
---
+++ src/shared/characters.js
@@ -0,0 +1,23 @@
+export const userStats = {
+    level: 44,
+    maxHealth: 177,
+    name: 'Cat',
+    img: '/assets/cat.png',
+
+    magic: 32,
+    attack: 50,
+    defense: 30,
+    magicDefense: 30,
+}
+
+export const opponentStats = {
+    level: 44,
+    maxHealth: 188,
+    name: 'Rat',
+    img: '/assets/rat.png',
+
+    magic: 50,
+    attack: 32,
+    defense: 20,
+    magicDefense: 48,
+}
